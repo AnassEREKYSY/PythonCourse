0938bddcc7723ceaac50a610e1a608a924e837c1
AnassEREKYSY <ereanass@gmail.com>
2023-12-04 14:30:16 +0100

    commit 1 HakckerRank

---
+++ HackerRank.py
@@ -0,0 +1,19 @@
+
+# exo1
+if __name__ == '__main__':
+    print("Hello, World!")
+
+# exo2
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    
+# exo3
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
